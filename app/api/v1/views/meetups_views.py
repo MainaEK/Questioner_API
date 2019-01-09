c5b2b6da7905244c1c0e20c66d417f5153f88cc0
Eric <eric_maina@hotmail.com>
2019-01-09 22:26:51 +0300

    [finishes  #163047222] completes the fetch specific meetup endpoint

--- app/api/v1/views/meetups_views.py
+++ app/api/v1/views/meetups_views.py
@@ -8,3 +8,20 @@ from ..utils.validations import sanitize_input
 def get_all_meetups():
     return jsonify({'status' : 201,'data' : meetups,}),201
 
+
+@v1.route('/meetups/<string:topic>', methods=['GET'])
+def get_specific_meetup(topic):
+    val = {}
+    for meetup in meetups:
+        if meetup['topic'] == topic:
+            val = {
+                'id_' : meetup['id_'],
+                'createdOn' : meetup['createdOn'],
+                'location' : meetup['location'],
+                'images' : meetup['images'],
+                'topic' : meetup['topic'],
+                'happeningOn' : meetup['happeningOn'],
+                'tags': meetup['tags']
+            }
+    return jsonify (val)    
+
