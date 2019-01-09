01c9076fda7f0ead79b008ec43a7ad8528d21812
Eric <eric_maina@hotmail.com>
2019-01-10 01:06:02 +0300

    [finishes #163046805] finishes the post questions endpoint

---
+++ app/api/v1/models/question_models.py
@@ -0,0 +1,29 @@
+
+questions =[ 
+{
+    'id_' : 1,
+    'createdOn' : '26-12-2018',
+    'createdBy' : '123',
+    'meetup' : '321',
+    'title' : 'Python',
+    'body' : 'string to integer',
+    'votes': '10'
+} ,
+{
+    'id_' : 2,
+    'createdOn' : '26-12-2018',
+    'createdBy' : '123',
+    'meetup' : '432',
+    'title' : 'Java',
+    'body' : 'string to integer',
+    'votes': '10'
+} ,
+{
+    'id_' : 3,
+    'createdOn' : '26-12-2018',
+    'createdBy' : '123',
+    'meetup' : '543',
+    'title' : 'Ruby',
+    'body' : 'string to integer',
+    'votes': '10'
+} ]
