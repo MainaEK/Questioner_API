3cc363cfb5815d87841cfdaab7472b131bba1121
Eric <eric_maina@hotmail.com>
2019-01-09 21:50:31 +0300

    [Feature  #163061043} adds the meetups_model

---
+++ app/api/v1/models/meetups_models.py
@@ -0,0 +1,29 @@
+
+meetups =[ 
+{
+    'id_' : 1,
+    'createdOn' : '26-12-2018',
+    'location' : 'Nairobi',
+    'images' : ['http://self.io'],
+    'topic' : 'Python',
+    'happeningOn' : '28-12-2018',
+    'tags': ['coding','python']
+} ,
+{
+    'id_' : 2,
+    'createdOn' : '27-12-2018',
+    'location' : 'Kiambu',
+    'images' : 'http://self.io',
+    'topic' : 'Java',
+    'happeningOn' : '29-12-2018',
+    'tags': ['coding','java']
+} ,
+{
+    'id_' : 3,
+    'createdOn' : '28-12-2018',
+    'location' : 'Nakuru',
+    'images' : 'http://self.io',
+    'topic' : 'Ruby',
+    'happeningOn' : '30-12-2018',
+    'tags': ['coding','ruby']
+} ]
--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os
 
 from app import create_app
 
-config_name = os.getenv("FLASK_ENV")
+config_name = os.getenv("APP_SETTINGS")
 
 app = create_app(config_name)
 
 if __name__ == "__main__":
-app.run(debug=True)
+   app.run(debug=True, port=8080)
