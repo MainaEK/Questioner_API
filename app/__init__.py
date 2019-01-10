cd83ea6e42a84856eb653951f2b9783429a75ebe
Eric <eric_maina@hotmail.com>
2019-01-10 22:39:03 +0300

    [finishes #163118083] sets up the environment again

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from instance.config import app_config
 from app.api.v1.views.meetups_views import v1 as meetups_blueprint
@@ -8,9 +9,10 @@ def create_app(config_name):
 
     # Initialize app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
+    app.config.from_object(app_config['development'])
     app.config.from_pyfile('config.py')
     app.register_blueprint(meetups_blueprint)
+    app.register_blueprint(questions_blueprint)
 
 
     return app
