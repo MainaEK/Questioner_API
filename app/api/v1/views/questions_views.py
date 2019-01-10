493c9039aeb69c1d27d7965b634286dc8776b0a8
Eric <eric_maina@hotmail.com>
2019-01-10 17:29:07 +0300

    [finishes #163112006] fixes the questions endpoints

--- app/api/v1/views/questions_views.py
+++ app/api/v1/views/questions_views.py
@@ -1,22 +1,18 @@
 from flask import Flask, jsonify, request, Response, json
-from ..models.question_models import questions
+from ..models.question_models import QuestionModel
 from ...v1 import v1 
 from ..utils.validations import sanitize_input_questions
 
-@v1.route('/questions', methods=['POST'])
-def create_question():
-    request_data = request.get_json()
-    if(sanitize_input_questions(request_data)):
-        new_question = {
-            'id_' : request_data['id_'],
-            'createdOn' : request_data['createdOn'],
-            'createdBy' : request_data['createdBy'],
-            'meetup' : request_data['meetup'],
-            'title' : request_data['title'],
-            'body' : request_data['body'],
-            'votes': request_data['votes']
-        }
-        questions.append(new_question)
-        return jsonify({'status' : 201,'data' : new_question}),201
-    else:
-        return jsonify({'status' : 400,'error' : 'Invalid Question passed'}),400
+
+class QuestionEndpoints(QuestionModel):
+    def __init__(self):
+        self.db = QuestionModel
+        
+    @v1.route('/questions', methods=['POST'])
+    def create_question(self):
+        data = request.get_json() ['question']
+        response = self.db.save(data)
+        if not response:
+            return jsonify({'status' : 400,'error' : 'Bad Request'}),400
+        else:
+            return jsonify ({'status' : 201,'data' : response}),201 
