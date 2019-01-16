9059a1576b790010fcf79deab5bac48124ae43cc
Eric <eric_maina@hotmail.com>
2019-01-16 20:33:04 +0300

    [starts #163046724] creating the login user test

--- app/tests/v1/test_users.py
+++ app/tests/v1/test_users.py
@@ -15,4 +15,13 @@ class TestQuestioner(unittest.TestCase):
         """ Test signing up a new user."""
         response = self.client.post('/api/v1/auth/signup', data=json.dumps(self.users), content_type = 'application/json')
         self.assertEqual(response.status_code, 201)
+
+    def test_login_user(self):
+        """ Test to login a user."""
+        self.users_detail = { "user_id":1,"firstname": "Eric", "lastname": "Maina","othername" : "EM","email": "maina_eric@hotmail.com",
+        "phone_number": "1234","username": "maina_eric", "registered_on": "2000-03-03", "password":"Erick1234"}
+        self.client.post('/api/v1/auth/signup', data=json.dumps(self.users_detail), content_type = 'application/json')
+        self.user_login = {"username": "maina_eric","password":"Erick1234"}
+        response = self.client.post('/api/v1/auth/login', data=json.dumps(self.user_login), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
         
