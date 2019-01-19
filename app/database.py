8838d1dfd9dc2eadf349c3e38587c0ac4061a498
Eric <eric_maina@hotmail.com>
2019-01-20 00:31:40 +0300

    [chore #163287362] changes the structure of the db connection

--- app/database.py
+++ app/database.py
@@ -1,27 +1,21 @@
 import psycopg2
 import os
 
+
 from dotenv import load_dotenv
-from  .db_tables import create_users,create_meetups,create_questions,create_comments,create_rsvps,create_votes
+
 
 load_dotenv()
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 
 def db_con():
-    con = psycopg2.connect(DATABASE_URL)
+    try:
+        con = psycopg2.connect(DATABASE_URL)
+
+    except Exception:
+        print("Unable to make database connection")
+    
     return con
-
-def create_db():
-    connect = db_con()
-    cursor = connect.cursor()
-    cursor.execute(create_users)
-    cursor.execute(create_meetups)
-    cursor.execute(create_questions)
-    cursor.execute(create_comments)
-    cursor.execute(create_rsvps)
-    cursor.execute(create_votes)
-    connect.commit()
-    cursor.close()
-
+    
 
