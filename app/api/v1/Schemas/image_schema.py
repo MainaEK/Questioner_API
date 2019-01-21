aa212d14882e246641bf5c051b27d7334a45c926
Eric <eric_maina@hotmail.com>
2019-01-21 19:25:34 +0300

    [starts #163356137] creates the directory structure for v2

--- app/api/v1/Schemas/image_schema.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-from ..utils.validations import Not_null_string
-
-
-class ImageSchema(Schema):
-    """ Class schema to validate images for Meetup object """
-
-    images = fields.List(fields.Str,required=True, validate = Not_null_string)
-    
