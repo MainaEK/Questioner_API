403b94eb8557377e26872d80a66a00e4511d6a62
Eric <eric_maina@hotmail.com>
2019-01-23 00:06:24 +0300

    [starts #163400929] added jwt to meetup views and refactored code

--- app/api/v2/models/rsvp_models.py
+++ app/api/v2/models/rsvp_models.py
@@ -6,11 +6,13 @@ class RsvpModel(BaseModels):
         super().__init__('rsvps')
     
         
-    def create_rsvp(self, m_id, rsvp):
+    def create_rsvp(self, user_id, m_id, rsvp):
         self.cur = self.connect.cursor()
-        query = """INSERT INTO rsvps (meetup_id,rsvp)\
-        VALUES ('{}','{}') RETURNING json_build_object('meetup_id',meetup_id,'topic',topic,'rsvp',rsvp)
-        ;""".format(m_id,rsvp)
+        query = """WITH inserted AS (INSERT INTO rsvps (user_id,meetup_id,rsvp)\
+        VALUES ('{}','{}','{}') RETURNING rsvp,meetup_id)\
+        SELECT json_build_object('rsvp',rsvp,'meetup_id',meetup_id,'topic',topic)\
+        FROM (SELECT inserted.rsvp,meetups.meetup_id,meetups.topic FROM inserted INNER JOIN meetups ON inserted.meetup_id = meetups.meetup_id) AS returned
+        ;""".format(user_id,m_id,rsvp)
         self.cur.execute(query)
         self.connect.commit()
         result = self.cur.fetchone()
