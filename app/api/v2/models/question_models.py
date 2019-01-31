f5989f99ebcb6a58b0744b3218e0c8d12f59564d
Eric <eric_maina@hotmail.com>
2019-01-31 21:49:22 +0300

    [Chore #163619159] restructures question model

--- app/api/v2/models/question_models.py
+++ app/api/v2/models/question_models.py
@@ -17,9 +17,8 @@ class QuestionModel(BaseModels):
         """
         Function to create a new question under a particular meetup
         """
-        self.cur = self.connect.cursor()
         query = """INSERT INTO questions (user_id,meetup_id,title,body)\
-        VALUES ('{}','{}','{}','{}') RETURNING json_build_object('user_id',user_id,'meetup_id',meetup_id,'question_id',question_id,'title',title,'body',body)
+        VALUES ('{}','{}','{}','{}') RETURNING user_id,meetup_id,question_id,title,body
         ;""".format(user_id, m_id, question['title'], question['body'])
         self.cur.execute(query)
         self.connect.commit()
@@ -29,8 +28,8 @@ class QuestionModel(BaseModels):
     def upvote(self, q_id):
         """Function to upvote a question"""
         self.cur = self.connect.cursor()
-        query = """UPDATE questions SET votes = votes + 1 WHERE question_id = {} RETURNING json_build_object('meetup_id',meetup_id,'title',title,'body',body,'votes',votes) ;""".format(
-            q_id)
+        query = """UPDATE questions SET votes = votes + 1 WHERE question_id = {} RETURNING meetup_id,title,body,votes
+        ;""".format(q_id)
         self.cur.execute(query)
         self.connect.commit()
         result = self.cur.fetchone()
@@ -39,8 +38,8 @@ class QuestionModel(BaseModels):
     def downvote(self, q_id):
         """Function to downvote a question"""
         self.cur = self.connect.cursor()
-        query = """UPDATE questions SET votes = votes - 1 WHERE question_id = {} RETURNING json_build_object('meetup_id',meetup_id,'title',title,'body',body,'votes',votes) ;""".format(
-            q_id)
+        query = """UPDATE questions SET votes = votes - 1 WHERE question_id = {} RETURNING meetup_id,title,body,votes
+        ;""".format(q_id)
         self.cur.execute(query)
         self.connect.commit()
         result = self.cur.fetchone()
