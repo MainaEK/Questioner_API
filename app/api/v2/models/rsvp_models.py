fd513f3a35d962b32769c14db0ba18a70629132d
Eric <eric_maina@hotmail.com>
2019-01-31 21:50:02 +0300

    [finishes #163619159] restructures rsvp model

--- app/api/v2/models/rsvp_models.py
+++ app/api/v2/models/rsvp_models.py
@@ -18,8 +18,7 @@ class RsvpModel(BaseModels):
         self.cur = self.connect.cursor()
         query = """WITH inserted AS (INSERT INTO rsvps (user_id,meetup_id,rsvp)\
         VALUES ('{}','{}','{}') RETURNING rsvp,meetup_id)\
-        SELECT json_build_object('rsvp',rsvp,'meetup_id',meetup_id,'topic',topic)\
-        FROM (SELECT inserted.rsvp,meetups.meetup_id,meetups.topic FROM inserted INNER JOIN meetups ON inserted.meetup_id = meetups.meetup_id) AS returned
+        SELECT inserted.rsvp,meetups.meetup_id,meetups.topic FROM inserted INNER JOIN meetups ON inserted.meetup_id = meetups.meetup_id
         ;""".format(user_id, m_id, rsvp)
         self.cur.execute(query)
         self.connect.commit()
